ab3cedc0ebf02dbfbb68b15593380e88b65d10ca
chiragmeanstack2020 <67252585+chiragmeanstack2020@users.noreply.github.com>
2020-06-22 09:33:21 +0530

    Add files via upload

---
+++ precache-manifest.99f27d87639a8e248d14be9046447ed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c0c20cbbc73819ffc4ea5d70c366e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d953dd7dd0f5b38c9b92",
+    "url": "/static/js/2.421806e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.421806e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4196cc93437a17d28702",
+    "url": "/static/js/main.df1d55b7.chunk.js"
+  },
+  {
+    "revision": "478e71c75e4f7e375bcc",
+    "url": "/static/js/runtime-main.d2dfff9f.js"
+  }
+]);
